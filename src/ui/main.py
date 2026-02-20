ab84212621ac433af7ea7dc5e81a1ff9252f44fb
karpetsmyroslav-arch <karpets.myroslav@lll.kpi.ua>
2026-02-20 23:34:25 +0200

    Changed buttons names

--- src/ui/main.py
+++ src/ui/main.py
@@ -7,12 +7,12 @@ from ui.windows.cycled_ui import CycledUI
 def main():
     root = tk.Tk()
 
-    root.title("Calculator")
+    root.title("Основний модуль")
     root.geometry("300x300")
 
-    tk.Button(root, text="Open", command=lambda: LinearUI(root)).grid()
-    tk.Button(root, text="Open", command=lambda: CurvedUI(root)).grid()
-    tk.Button(root, text="Open", command=lambda: CycledUI(root)).grid()
+    tk.Button(root, text="Лінійний", command=lambda: LinearUI(root)).grid()
+    tk.Button(root, text="Розгалужений", command=lambda: CurvedUI(root)).grid()
+    tk.Button(root, text="Циклічний", command=lambda: CycledUI(root)).grid()
 
     root.mainloop()
 
